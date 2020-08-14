295f13f566289178def5298e0a858204b70be151
TheDariaEdits <thefashionphysician@gmail.com>
2020-08-14 02:39:38 -0400

    Pixel data complete

--- Hip Hop script.R
+++ Hip Hop script.R
@@ -18,5 +18,36 @@ img <- as.numeric(img[[1]])
 #Check dimensions of bitmap image 
 dim(img)
 
+img <- img[,,1] 
+
+#Number of zeros 
+n_zeros <- length(img[img == 0])
+n_zeros
+
+#Down sample, this takes some time due to the amount of down sampling required
+targetPixels <- 311
+for (i in seq(1,1000,0.0001)){
+  
+  img2 <- down_sample_image(img, i)
+  n_zeros2 <- length(img2[img2 == 0])
+  
+  if (n_zeros2 == targetPixels){
+    break
+  } 
+  
+}
+
+
 #Check that image is working 
-image(img)
+image(img2)
+
+# Rotate image
+rotate <- function(x) t(apply(x, 2, rev))
+img2 = rotate(img2)
+
+# Extract matrix coordinates of 0's
+coords <- as.data.frame(which(img2 == 0, arr.ind=TRUE))
+names(coords) <- c("PixelX", "PixelY")
+
+#Send coords to a csv file
+write_csv(coords,"/Users/dariamalone/Documents/Code/Hip-Hop/Coords.csv", col_names = TRUE)
